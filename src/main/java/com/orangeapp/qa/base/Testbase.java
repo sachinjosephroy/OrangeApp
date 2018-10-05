a3c922557ebf9e9146a27ca85c1341905c0aea30
Sachin Roy <sachin.jr@gmail.com>
2018-10-05 12:46:09 -0400

    Add code to invoke browser

--- src/main/java/com/orangeapp/qa/base/Testbase.java
+++ src/main/java/com/orangeapp/qa/base/Testbase.java
@@ -2,6 +2,7 @@ package com.orangeapp.qa.base;
 
 import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -43,6 +44,11 @@ public class Testbase {
 			System.setProperty("webdriver.gecko.driver", prop.getProperty("driverpath"));
 			driver = new FirefoxDriver();
 		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
 	}
 	
 	public void initializePages() {
